db6076989fa95e2cb367ea20817b683dd2571fd5
SayoTeKey <sayo.briefs@gmail.com>
2025-03-03 21:19:41 +0100

    adding plants components and -data; improving globale CSS

---
+++ src/assets/data/data.jsx
@@ -0,0 +1,39 @@
+const fiktivePflanzen = [
+  {
+    name: "Luminaria nocturna",
+    farbe: "Leuchtendes Blau",
+    standort: "Dunkle Höhlen",
+    besonderheit: "Gibt nachts ein sanftes Licht ab",
+    bild: "https://images.pexels.com/photos/230129/pexels-photo-230129.jpeg",
+  },
+  {
+    name: "Flammenranke",
+    farbe: "Feuriges Rot",
+    standort: "Vulkanränder",
+    besonderheit: "Ihre Blätter glühen bei Hitze",
+    bild: "https://images.pexels.com/photos/230129/pexels-photo-230129.jpeg",
+  },
+  {
+    name: "Frostblüte",
+    farbe: "Eisiges Weiß",
+    standort: "Hochgebirge",
+    besonderheit: "Senkt die Temperatur ihrer Umgebung",
+    bild: "https://images.pexels.com/photos/230129/pexels-photo-230129.jpeg",
+  },
+  {
+    name: "Nebelmoos",
+    farbe: "Blasses Grün",
+    standort: "Sumpfgebiete",
+    besonderheit: "Verströmt einen dichten Nebel",
+    bild: "https://images.pexels.com/photos/230129/pexels-photo-230129.jpeg",
+  },
+  {
+    name: "Schattenkelch",
+    farbe: "Tiefes Violett",
+    standort: "Dunkelwälder",
+    besonderheit: "Kann sich nahezu unsichtbar machen",
+    bild: "https://images.pexels.com/photos/230129/pexels-photo-230129.jpeg",
+  },
+];
+
+export default fiktivePflanzen;
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,8 +1,32 @@
+import "./Home.css";
 import React from "react";
+import fictionalPlants from "../../../assets/data/data";
+import { useState } from "react";
 
 const Home = () => {
-  return <h1>My Garden</h1>;
-  <h2>Home</h2>;
+  const [plants, setPlants] = useState(fictionalPlants);
+
+  console.log(plants);
+
+  return (
+    <>
+      <h1>My Garden</h1>
+      <h2>Home</h2>
+
+      <h3>Plants</h3>
+      <div className="plants-container">
+        {plants.map((plant) => (
+          <div className="card">
+            <img className="pic" src={plant.bild} alt={plant.name} />
+            <h3>{plant.name}</h3>
+            <p>{plant.farbe}</p>
+            <p>{plant.standort}</p>
+            <p>{plant.besonderheit}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Home;
